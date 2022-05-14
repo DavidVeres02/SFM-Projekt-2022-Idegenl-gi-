17377117216591f2fa170a666cb73476f50bef17
DEarniroth <101334675+DEarniroth@users.noreply.github.com>
2022-05-14 11:12:33 +0200

    project update 2
    Mostmár ki is néz valahogy

--- Bakancsos/src/main/java/hu/unideb/inf/FXMLLogPageController.java
+++ Bakancsos/src/main/java/hu/unideb/inf/FXMLLogPageController.java
@@ -1,4 +1,7 @@
 package hu.unideb.inf;
+import hu.unideb.inf.model.JPAUsersDAO;
+import hu.unideb.inf.model.Users;
+import hu.unideb.inf.model.UsersDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +11,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FXMLLogPageController {
 
@@ -36,8 +41,32 @@ public class FXMLLogPageController {
         }
 
         @FXML
-        void HandledLoginButtonPushed(ActionEvent event) {
+        void HandledLoginButtonPushed(ActionEvent event) throws IOException {
+            if (ValidateUser(user.getText(), pass.getText())) {
+                FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/FXMLMenuPage.fxml"));
+                Scene scene = new Scene(loader.load());
+                Stage stage = new Stage();
+                Stage stage2 = (Stage) ButtonBack.getScene().getWindow();
+                stage2.close();
+                stage.setTitle("Main Page");
+                stage.setScene(scene);
+                stage.show();
+            }
+        }
 
+        private boolean ValidateUser(String username, String password){
+            try(UsersDAO aDAO = new JPAUsersDAO();) {
+                List<Users> UsersList = new ArrayList<>(aDAO.getPersons());
+                for (Users user : UsersList) {
+                    if (user.getUsername().equals(username) && user.getPassword().equals(password)){
+                        return true;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return false;
         }
 
     }
